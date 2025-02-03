2c4fcda097b2b1e51d7037722ae6cafb73a99387
Demetrius Sullivan <dssullivan1@gmail.com>
2025-02-03 11:58:24 -0500

    added our coffee bean background image on the top and added our avatar image

--- app/(tabs)/HomeScreen.styles.ts
+++ app/(tabs)/HomeScreen.styles.ts
@@ -9,7 +9,20 @@ const styles = StyleSheet.create({
     },
     text: {
         color: 'red',
-    }
+    },
+
+    backgroundImage: {
+        position: 'absolute',
+        top: 0,
+        width: '100%',
+        height: 220, // Adjust height as needed
+      },
+
+      topIconsWrap: {
+         display: 'flex', 
+      }
+
+
   });
 
 export default styles;
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 import styles from './HomeScreen.styles';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 
 const HomeScreen = () => {
   return (
     <View style={styles.container}>
+      <Image source={require('../../assets/images/coffee-bean-background.png')}
+      style={styles.backgroundImage}/>
       <Text style={styles.text}>Welcome to the Home Screen!</Text>
+
+
+    <SafeAreaView className={styles.topIconsWrap}>
+      <Image source={require('../../assets/images/empty-avatar.jpg')}
+      style={styles.backgroundImage}/>
+    </SafeAreaView>
+
+
     </View>
+
+
   );
 };
 
