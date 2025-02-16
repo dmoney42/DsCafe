3ddb1165a9232ad5e92a85898433945b90d36553
Demetrius Sullivan <dssullivan1@gmail.com>
2025-02-16 12:55:56 -0500

    we added more styles to our ProductScreen screen. we added the back button to navigate to the previous screen we came from, the heart button to favorite a coffee dring, added coffee background, drink image, price and product name

--- app/(tabs)/ProductScreen.styles.ts
+++ app/(tabs)/ProductScreen.styles.ts
@@ -4,6 +4,95 @@ const productStyles = StyleSheet.create({
 
     productDetailWrap: {
         flex: 1,
+    },
+
+    productBackgroundImage:{
+        height: 200, 
+        width: "100%",
+        position:'absolute',
+        borderBottomLeftRadius: 50, 
+        borderBottomRightRadius: 50,
+        opacity: 0.9,
+    },
+
+    topIconsContainer: {
+        marginVertical: 16,
+    },
+
+    topIconsContainerChild1: {
+        marginHorizontal: 16,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+
+    heartButton: {
+        borderRadius: 9999,
+        borderWidth: 2,
+        borderColor: 'white',
+        padding: 8,
+        
+    },
+
+    productScreenCoffeeImageContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+    },
+
+    coffeeImage: {
+        height: 250,
+        width: 285,
+    },
+
+    reviewContainer: {
+        backgroundColor: 'rgb(194, 125, 72)',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderRadius: 12, // rounded-3x1 (assuming 1rem = 4px)
+        marginHorizontal: 8,
+        padding: 4, //tailwind p-1 (assuming 1rem = 4px)
+        paddingHorizontal: 8, //tailwind px-2
+        width: 50,
+        opacity: 0.9,
+    },
+
+    reviewContainerText: {
+        fontSize: 16, // Assuming 1rem = 16px for text-base
+        fontWeight: 'bold',
+        color: 'white',
+    },
+
+    priceContainer: {
+        marginVertical: 16,
+        display: 'flex',
+        flexDirection: 'row', //
+        justifyContent: 'space-between', //justify-between in tailwind
+        alignItems: 'center', //items-center in tailwind
+    },
+
+    priceContainerItemName: {
+        color: '#724015',
+        fontSize: 24, 
+        fontWeight: 600,       
+    },
+
+    priceContainerPrice: {
+        color: '#724015',
+        fontSize: 18, // Assuming 1rem = 18px for tailwind property called text-lg
+
+    },
+
+    coffeeSizeWrap:{
+        marginVertical: 16,
+        marginHorizontal: 8,
+    },
+
+    coffeeSizeText: {
+        color: '#724015',
+        fontSize: 18, // Assuming 1rem = 18px for text-lg
+        fontWeight: 'bold',
     }
 
 });
--- app/(tabs)/ProductScreen.tsx
+++ app/(tabs)/ProductScreen.tsx
@@ -1,16 +1,57 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import productStyles from './ProductScreen.styles';
 import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useNavigation } from '@react-navigation/native';
+import { ArrowLeftCircleIcon } from 'react-native-heroicons/outline';
+import { HeartIcon, StarIcon } from 'react-native-heroicons/solid';
 
-export default function ProductScreen() {
+export default function ProductScreen(props) {
+    const item = props.route.params;
+    const navigation = useNavigation();
   return (
     <View style={productStyles.productDetailWrap}>
       <StatusBar style="light" />
       <Image source={require('../../assets/images/product-background.jpg')} 
-      style={{height: 200, boroderBttomLeftRadius: 50, borderBottomRightRadius: 50}}
+      style={productStyles.productBackgroundImage}
       />
       <Text>ProductScreen</Text>
+      <SafeAreaView style={productStyles.topIconsContainer}>
+        <View style={productStyles.topIconsContainerChild1}>
+            <TouchableOpacity onPress={()=> navigation.goBack()}>
+                <ArrowLeftCircleIcon size="50" strokeWidth={1.8} color="white" />
+            </TouchableOpacity>
+
+
+            <TouchableOpacity style={productStyles.heartButton}>
+                <Text><HeartIcon size="24" color="white" /></Text>
+            </TouchableOpacity>
+        </View>
+
+        <View style={productStyles.productScreenCoffeeImageContainer}>
+            <Image style={productStyles.coffeeImage} source={item.image} />
+        </View>
+
+        <View style={productStyles.reviewContainer}>
+            <Text><StarIcon size="15" color="white" /></Text>
+            <Text style={productStyles.reviewContainerText}>{item.stars}</Text>
+        </View>
+
+        <View style={productStyles.priceContainer}>
+            <Text style={productStyles.priceContainerItemName}>{item.name}</Text>
+
+            <Text style={productStyles.priceContainerPrice}>${item.price}</Text>
+        </View>
+
+        <View style={productStyles.coffeeSizeWrap}>
+            <Text style={productStyles.coffeeSizeText}>
+                Coffee Size
+            </Text>
+        </View>
+
+
+      </SafeAreaView>
     </View>
   )
 }
